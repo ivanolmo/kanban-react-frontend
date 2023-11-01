4e4ccab0c350d493c9d544bdcab712f7470187b2
Ivan Olmo <ivan.olmo@gmail.com>
2023-10-31 22:44:05 -0500

    Update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,43 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.tsx"],
+  darkMode: "class",
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      black: "#000000",
+      blue: "#0827f5",
+      gunmetal: {
+        700: "#3e3f4e",
+        800: "#2b2c37",
+      },
+      indigo: "#e4ebfa",
+      red: {
+        400: "#ff9898",
+        600: "#ea5555",
+        900: "#ff0000",
+      },
+      richBlack: "#000112",
+      slate: "#828fa3",
+      violet: {
+        50: "#f4f7fd",
+        400: "#a8a4ff",
+        700: "#635fc7",
+      },
+      yellow: "#ffb81c",
+      zinc: "#20212c",
+      gradient: "rgba(0, 0, 0, 0.7)",
+    },
+    fontSize: {
+      xl: "1.5rem",
+      lg: "1.125rem",
+      md: "0.9375rem",
+      sm: "0.75rem",
+      "body-lg": ["0.8125rem", "1.75"],
+      "body-md": "0.75rem",
+    },
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
