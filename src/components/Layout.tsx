939c6e72ca4553da7aee925d247ed0b10a6776f9
Ivan Olmo <ivan.olmo@gmail.com>
2023-11-02 12:36:23 -0500

    Remove unused login and signup pages

--- src/components/Layout.tsx
+++
@@ -1,7 +0,0 @@
-import { type PropsWithChildren } from "react";
-
-const Layout = (props: PropsWithChildren) => {
-  return <div className="flex flex-col gap-4 p-4">{props.children}</div>;
-};
-
-export default Layout;
