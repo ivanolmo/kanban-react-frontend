997912ee4c1872ca7aeb46f42547b2a0e338d529
Ivan Olmo <ivan.olmo@gmail.com>
2023-11-03 13:34:29 -0500

    Add types for entities and API response to dedicated directory

---
+++ src/types/index.ts
@@ -0,0 +1,46 @@
+export type Board = {
+  id: string;
+  userId: string;
+  name: string;
+  columns: Column[];
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type Column = {
+  id: string;
+  boardId: string;
+  accentColor: string;
+  name: string;
+  tasks: Task[];
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type Task = {
+  id: string;
+  columnId: string;
+  title: string;
+  description: string;
+  completed: boolean;
+  subtasks: Subtask[];
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type Subtask = {
+  id: string;
+  taskId: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type ApiBoardResponse = {
+  data: Board[];
+  success: boolean;
+  message: string;
+  error: null | string;
+  status: string;
+};
