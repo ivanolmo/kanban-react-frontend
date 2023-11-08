eb0901eb89a9d6aef3f3ad35f930e740a6a93423
Ivan Olmo <ivan.olmo@gmail.com>
2023-11-08 01:20:30 -0600

    Add new custom Select component used to view a task
    This custom Select is built with headlessui and is customized to allow a task column to be changed by selecting a different column from the dropdown menu.

---
+++ src/components/ui/Select.tsx
@@ -0,0 +1,141 @@
+import { Fragment, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Listbox, Transition } from "@headlessui/react";
+import {
+  useController,
+  type Control,
+  type FieldPath,
+  type FieldValues,
+} from "react-hook-form";
+import clsx from "clsx";
+import { selectCurrentBoard, selectShowEditTaskModal } from "~/store/selectors";
+import { selectCurrentTask } from "~/store/selectors";
+import ChevronDownIcon from "../svg/ChevronDownIcon";
+import CheckIcon from "../svg/CheckIcon";
+import { toggleEditTaskModal } from "~/store/uiSlice";
+
+type SelectProps<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+> = {
+  name: TName;
+  control: Control<TFieldValues>;
+  handleColumnMove?: (columnId: string, taskId: string) => void;
+};
+
+const Select = <
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+>(
+  props: SelectProps<TFieldValues, TName>,
+): JSX.Element => {
+  const dispatch = useDispatch();
+  const currentBoard = useSelector(selectCurrentBoard);
+  const currentTask = useSelector(selectCurrentTask);
+  const showEditTaskModal = useSelector(selectShowEditTaskModal);
+
+  const {
+    field: { value, onChange },
+  } = useController(props);
+
+  const { handleColumnMove } = props;
+
+  // on mount set default select value to todo if new task, or current column if viewing task
+  useEffect(() => {
+    if (!currentTask) {
+      onChange(currentBoard?.columns[0]);
+    } else {
+      onChange(
+        currentBoard?.columns.find(
+          (column) => column.id === currentTask?.columnId,
+        ),
+      );
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentBoard?.columns, currentTask]);
+
+  // this fires if existing task and user changes column
+  useEffect(() => {
+    if (
+      currentTask &&
+      !showEditTaskModal &&
+      Object.keys(value).length &&
+      value.id !== currentTask?.columnId
+    ) {
+      handleColumnMove && handleColumnMove(value.id as string, currentTask?.id);
+      dispatch(toggleEditTaskModal());
+      console.log("edit task modal toggle");
+    }
+  }, [currentTask, dispatch, handleColumnMove, showEditTaskModal, value]);
+
+  return (
+    <Listbox value={value} onChange={onChange}>
+      {({ open }) => (
+        <div className="relative">
+          <Listbox.Button className="relative w-full cursor-pointer rounded-md border border-slate/25 px-4 py-2 text-left text-body-lg">
+            <span>{value.name ?? currentBoard?.columns[0]?.name}</span>
+            <span
+              className={clsx(
+                "absolute right-4 top-4 cursor-pointer transition",
+                open && "rotate-180",
+              )}
+            >
+              <ChevronDownIcon className="stroke-violet-700" />
+            </span>
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in-out duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="absolute w-full overflow-auto rounded-md bg-white text-body-lg shadow-xl dark:bg-zinc">
+              {currentBoard?.columns.map((column) => {
+                return (
+                  <Listbox.Option
+                    key={column.id}
+                    className={({ active }) =>
+                      clsx(
+                        "group relative cursor-pointer px-4 py-2",
+                        active ? "bg-violet-700 text-white" : "text-slate",
+                        value.id === column.id
+                          ? "bg-violet-700 text-white"
+                          : "text-slate",
+                      )
+                    }
+                    value={{
+                      id: column.id,
+                      name: column.name,
+                    }}
+                  >
+                    {() => (
+                      <>
+                        <span
+                          className={clsx(
+                            "block",
+                            value.id === column.id
+                              ? "font-bold"
+                              : "font-normal",
+                          )}
+                        >
+                          {column.name}
+                        </span>
+                        {value.id === column.id && (
+                          <span className="absolute right-4 top-4 stroke-white">
+                            <CheckIcon />
+                          </span>
+                        )}
+                      </>
+                    )}
+                  </Listbox.Option>
+                );
+              })}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      )}
+    </Listbox>
+  );
+};
+
+export default Select;
