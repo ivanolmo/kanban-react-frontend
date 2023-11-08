f66a4b1e2c43c44a3db7eeddd7391ed949237898
Ivan Olmo <ivan.olmo@gmail.com>
2023-11-08 13:55:33 -0600

    Fix stroke-width attribute -> strokeWidth

--- src/components/svg/CheckIcon.tsx
+++ src/components/svg/CheckIcon.tsx
@@ -5,7 +5,7 @@ const CheckIcon: React.FC<ComponentProps<"svg">> = (props) => {
     <svg width="10" height="8" xmlns="http://www.w3.org/2000/svg" {...props}>
       <path
         stroke="current"
-        stroke-width="2"
+        strokeWidth="2"
         fill="none"
         d="m1.276 3.066 2.756 2.756 5-5"
       />
--- src/components/svg/ChevronDownIcon.tsx
+++ src/components/svg/ChevronDownIcon.tsx
@@ -3,7 +3,7 @@ import type { ComponentProps } from "react";
 const ChevronDownIcon: React.FC<ComponentProps<"svg">> = (props) => {
   return (
     <svg width="10" height="7" xmlns="http://www.w3.org/2000/svg" {...props}>
-      <path stroke="current" stroke-width="2" fill="none" d="m1 1 4 4 4-4" />
+      <path stroke="current" strokeWidth="2" fill="none" d="m1 1 4 4 4-4" />
     </svg>
   );
 };
