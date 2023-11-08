67d45963fb6fa5fc8b5be4f66a55c2831679ace2
Ivan Olmo <ivan.olmo@gmail.com>
2023-11-08 17:50:55 -0600

    Add new DeleteTask modal

---
+++ src/components/modal/DeleteTask.tsx
@@ -0,0 +1,55 @@
+import { useDispatch, useSelector } from "react-redux";
+
+import Button from "~/components/ui/Button";
+import { useDeleteTaskMutation } from "~/store/api";
+import { clearCurrentTask } from "~/store/boardSlice";
+import { selectCurrentTask } from "~/store/selectors";
+import { toggleDeleteTaskModal } from "~/store/uiSlice";
+
+const DeleteTask: React.FC = () => {
+  const [deleteTask, { isLoading, error, data }] = useDeleteTaskMutation();
+  const dispatch = useDispatch();
+
+  const currentTask = useSelector(selectCurrentTask);
+
+  const handleDelete = async () => {
+    try {
+      await deleteTask(currentTask!.id).unwrap();
+      dispatch(toggleDeleteTaskModal());
+      dispatch(clearCurrentTask());
+    } catch (err) {
+      console.log("err -> ", err);
+    }
+  };
+
+  if (error) return <p>Error</p>;
+
+  if (isLoading) return <p>Deleting task...</p>;
+
+  return (
+    <div className="z-[99999] flex flex-col gap-6">
+      <h2 className="text-red-600">Delete this task?</h2>
+      <p className="text-body-lg text-slate">
+        Are you sure you want to delete the{" "}
+        <span className="capitalize">&apos;{currentTask?.title}&apos;</span>{" "}
+        task? This action will remove all{" "}
+        <span className="font-bold">{currentTask?.subtasks?.length}</span>{" "}
+        subtasks and cannot be reversed!
+      </p>
+      <div className="flex flex-col gap-4 md:flex-row">
+        <Button variant="delete" wide onClick={() => handleDelete()}>
+          <span>Delete</span>
+        </Button>
+        <Button
+          variant="secondary"
+          wide
+          onClick={() => dispatch(toggleDeleteTaskModal())}
+        >
+          <span>Cancel</span>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteTask;
