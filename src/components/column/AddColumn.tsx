aff180e03e50fc12701b08a6ad2a0783c9413505
Ivan Olmo <ivan.olmo@gmail.com>
2023-11-07 22:35:01 -0600

    Refactor component to add EditBoard toggle to onClick

--- src/components/column/AddColumn.tsx
+++ src/components/column/AddColumn.tsx
@@ -1,10 +1,15 @@
+import { useDispatch } from "react-redux";
+
+import { toggleEditBoardModal } from "~/store/uiSlice";
 import AddIcon from "~/components/svg/AddIcon";
 
 const AddColumn: React.FC = () => {
+  const dispatch = useDispatch();
+
   return (
     <div
       className="group mt-10 flex h-[40rem] w-72 flex-shrink-0 cursor-pointer flex-col items-center justify-center rounded-md bg-indigo shadow-md transition-all hover:scale-105 hover:shadow-xl dark:bg-gunmetal-800"
-      onClick={() => console.log("add column")}
+      onClick={() => dispatch(toggleEditBoardModal())}
     >
       <div className="flex items-center gap-2">
         <AddIcon className="fill-slate transition-all group-hover:rotate-180 group-hover:scale-125" />
