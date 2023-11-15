36aac5b09042fc18964e6abba038b38e591d68d9
Ivan Olmo <ivan.olmo@gmail.com>
2023-11-14 23:39:15 -0600

    Remove unused file

--- src/utils/transformers.ts
+++
@@ -1,7 +0,0 @@
-import type { ApiBoardResponse, Board } from "~/types";
-
-// Utility function to transform ApiBoardResponse to a consistent frontend type
-export const transformApiResponse = (response: ApiBoardResponse): Board[] => {
-  // Ensure that the data is always returned as an array
-  return Array.isArray(response.data) ? response.data : [response.data];
-};
