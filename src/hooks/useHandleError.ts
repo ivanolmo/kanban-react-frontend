5f17eca82305777c63638ee7c39248aeae1e89d6
Ivan Olmo <ivan.olmo@gmail.com>
2023-11-20 02:02:42 -0600

    Add custom hook for global error handling
    This hook is used in any component where an error can occur, which is anywhere queries or mutations are being used.
    
    If an error occurs, this hook sets the error message in the redux store and displays an error modal.

---
+++ src/hooks/useHandleError.ts
@@ -0,0 +1,26 @@
+import type { SerializedError } from "@reduxjs/toolkit";
+import type { FetchBaseQueryError } from "@reduxjs/toolkit/query";
+import { useDispatch } from "react-redux";
+
+import { setError } from "~/store/uiSlice";
+import type { ApiErrorResponse } from "~/types";
+
+export const useHandleError = () => {
+  const dispatch = useDispatch();
+
+  const handleError = (error: FetchBaseQueryError | SerializedError) => {
+    if ("status" in error) {
+      const apiError = error.data as ApiErrorResponse;
+
+      if (apiError && apiError.message) {
+        dispatch(setError(apiError.message));
+      } else {
+        dispatch(setError("An unexpected server error occurred"));
+      }
+    } else {
+      dispatch(setError(error.message ?? "An unexpected error occurred"));
+    }
+  };
+
+  return handleError;
+};
