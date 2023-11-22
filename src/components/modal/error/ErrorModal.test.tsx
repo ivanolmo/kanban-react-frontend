4aef4b8bae59459b59674a74e1cb61ee1bb4a7bd
Ivan Olmo <ivan.olmo@gmail.com>
2023-11-21 23:01:46 -0600

    Add tests for ErrorModal component

---
+++ src/components/modal/error/ErrorModal.test.tsx
@@ -0,0 +1,55 @@
+import {
+  act,
+  fireEvent,
+  renderWithProviders,
+  waitFor,
+  screen,
+} from "~/utils/testUtils";
+import { clearError, initialUIState } from "~/store/uiSlice";
+
+import ErrorModal from "~/components/modal/error/ErrorModal";
+
+describe("ErrorModal", () => {
+  test("modal renders correctly", () => {
+    const { getByText } = renderWithProviders(<ErrorModal />);
+
+    expect(getByText("Something went wrong")).toBeInTheDocument();
+    expect(getByText("OK")).toBeInTheDocument();
+  });
+
+  test("displays the error message from the Redux store", () => {
+    const preloadedState = {
+      ui: {
+        ...initialUIState,
+        errorMessage: "Test error message",
+      },
+    };
+
+    renderWithProviders(<ErrorModal />, { preloadedState });
+
+    expect(
+      screen.getByText("Test error message! Please try again."),
+    ).toBeInTheDocument();
+  });
+
+  test("dispatches clearError action when OK button is clicked", async () => {
+    const preloadedState = {
+      ui: {
+        ...initialUIState,
+        errorMessage: "Test error message",
+      },
+    };
+
+    const { getByText, dispatch } = renderWithProviders(<ErrorModal />, {
+      preloadedState,
+    });
+
+    act(() => {
+      fireEvent.click(getByText(/Ok/i));
+    });
+
+    await waitFor(() => {
+      expect(dispatch).toHaveBeenCalledWith(clearError());
+    });
+  });
+});
